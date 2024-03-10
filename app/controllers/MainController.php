beb29b15ae6e996216daa57b41e58557b9805fc8
MatthewMalefyt <157759162+MatthewMalefyt@users.noreply.github.com>
2024-03-09 23:58:23 -0500

    Update MainController.php

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -16,11 +16,31 @@ class MainController extends Controller
 
         echo $template->render($homepageData);
     }
+<?php
+
+namespace app\controllers;
+use app\core\Controller;
+use app\models\User;
+
+    //an example is in app/controllers/UsersController
+    public function index()
+    {
+        $userModel = new User();
+        //the load method will start at the public/assets/views directory
+        //you can view the set up in the app/core/Controller class
+        $template = $this->twig->load('users/users.twig');
+        $homepageData = [
+            'users' => $userModel->getAllUsers(),
+        ];
+        echo $template->render($homepageData);
+    }
+
+}
 
     public function notFound() {
         //todo create a 404 twig template in app/public/assets/views
-        //an example is in app/controllers/UsersController
         //and return it from this method
+        return index(); 
     }
 
 }
