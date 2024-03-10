e9dabe07eeebf8c5a6a21203b454c40e6d570b2e
MatthewMalefyt <157759162+MatthewMalefyt@users.noreply.github.com>
2024-03-10 14:47:57 -0400

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,5 +4,10 @@ namespace app\models;
 
 class Post
 {
-
+  $connection = include('dbConnection.php');
+$name = $_POST['name'];
+$query = mysqli_query($connection, "SELECT * FROM clients WHERE name = 
+    $name");
+$result = mysqli_fetch_array($query);
+return $result;
 }
