656afb68b5d18ab99da680f6ec020dcca2754f04
MatthewMalefyt <157759162+MatthewMalefyt@users.noreply.github.com>
2024-03-10 17:42:32 -0400

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,10 +4,5 @@ namespace app\models;
 
 class Post
 {
-  $connection = include('dbConnection.php');
-$name = $_POST['name'];
-$query = mysqli_query($connection, "SELECT * FROM clients WHERE name = 
-    $name");
-$result = mysqli_fetch_array($query);
-return $result;
+  return array("brand"=>"Ford", "model"=>"Mustang", "year"=>1964);
 }
