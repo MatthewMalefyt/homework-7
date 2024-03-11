7a6f2b059ab44ef3e93642a03a7572bf315fb9ca
MatthewMalefyt <157759162+MatthewMalefyt@users.noreply.github.com>
2024-03-10 21:42:33 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -9,10 +9,25 @@ use app\controllers\MainController;
 use app\controllers\UserController;
 use app\controllers\PostController;
 
-
+// superglobal to get information from the server 
 $url = $_SERVER["REQUEST_URI"];
+//echo($url); 
 
 //todo add a switch statement router to route based on the url
-//if it is "/posts" return an array of posts via the post controller
-//if it is "/" return the homepage view from the main controller
-//if it is something else return a 404 view from the main controller
+switch($url) {
+    //if it is "/posts" return an array of posts via the post controller
+    case "/posts":
+        $postController = new PostController(); 
+        $posts = $postController->showPosts(); 
+        break;
+    //if it is "/" return the homepage view from the main controller
+    case "/":
+        $mainController = new MainController(); 
+        $mainController->homePage(); 
+        break; 
+    //if it is something else return a 404 view from the main controller
+    default:
+        $mainController = new MainController(); 
+        $mainController->notFound(); 
+        break;    
+}
